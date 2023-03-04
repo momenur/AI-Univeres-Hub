bfbfb04b2ca43f0ac38e4964a52f0904028e1ab3
momenur <momenurislam6@gmail.com>
2023-03-04 21:59:19 +0600

    added modal accuracy

--- js/index.js
+++ js/index.js
@@ -5,44 +5,67 @@ const baseEndpoint = 'https://openapi.programming-hero.com/api'
 
 let allData = []
 let isLoadAllData = false
+let modalLoading = false
 
-const innerFeatures = (features) => {
+const innerFeatures = (array = []) => {
   let innerHTML = ''
-  features.map(item => {
-    innerHTML += `<li>${item}</li>`
+  if (array.length) {
+    array.map(item => {
+      innerHTML += `<li>${item}</li>`
+    })
+  } else innerHTML = 'No data found'
+  return innerHTML
+}
+
+const innerModalFeatures = (features) => {
+  let innerHTML = ''
+
+  for (const item in features) {
+    innerHTML += `<li>${features[item].feature_name}</li>`
+  }
+
+  return innerHTML
+}
+
+const pricingFeature = (pricing) => {
+  let innerHTML = ''
+  pricing.map((item, index) => {
+    const price = parseInt(item.price) !== 0
+    let plan = ''
+
+    if (!index) plan = 'of Cost/Basic'
+    else if (index == 1) plan = 'of Cost/Pro'
+    else plan = 'Free of Cost /Enterprise'
+
+    innerHTML += `<div class="card" style="padding:6px; margin: 6px;">
+                   ${item.plan}  ${price ? item.price : plan}
+                   </div>`
   })
   return innerHTML
 }
 
 const renderModalData = (item) => {
+  console.log({ item })
   const cardsContainer = document.getElementById('modalBody')
   let innerHTMLData = `<div class="col-sm-6">
                 <div class="card h-100">
                   <div style="background: #f2ecec" class="card-body">
                     <h5>${item.description}</h5>
                     <div class="d-flex">
-                      <div class="card" style="padding:6px; margin: 6px;">
-                        Free of Cost/Basic
-                      </div>
-                      <div class="card" style="padding:6px; margin: 6px;">
-                        Free of Cost/Pro
-                      </div>
-                      <div class="card" style="padding:6px; margin: 6px;">
-                        Free of Cost/Enterprise
-                      </div>
+                    ${pricingFeature(item.pricing)}
                     </div>
 
                     <div class="d-flex d-flex justify-content-between">
                       <div >
                         <h6 class="card-title">Features</h6>
                         <ul style="font-size: small; color: dimgray;">
-                        ${innerFeatures(item.features)}
+                        ${innerModalFeatures(item.features)}
                         </ul>
                       </div>
                       <div>
                         <h6 class="card-title">Integration</h6>
                         <ul style="font-size: small; color: dimgray;">
-                        ${item.integrations ? innerFeatures(item.integration) : 'No data found'}
+                        ${innerFeatures(item.integrations)}
                         </ul>
                       </div>
                     </div>
@@ -51,11 +74,12 @@ const renderModalData = (item) => {
               </div>
               <div class="col-sm-6">
                 <div class="card h-100">
-                  <img src=${item.image}
-                    class="card-img-top" alt="..." >
+                  <img src=${item.image_link[0]}
+                    class="card-img-top" alt="...">
+                    <p class="top-right"}>${item.accuracy.score * 100}% accuracy</p>
                   <div align="center">
-                    <h6 class="card-title">Can you give any example?</h6>
-                    <p>No! Not yet! Take a break</p>
+                    <h6 class="card-title">${item.input_output_examples[0]?.input}</h6>
+                    <p>${item.input_output_examples[0]?.output}</p>
                   </div>
                 </div>
               </div>`
@@ -63,16 +87,19 @@ const renderModalData = (item) => {
 }
 
 const showToolModal = (id) => {
-  fetch(`${baseEndpoint}/ai/tool/${id}`)
+  const toolId = id <= 9 ? `0${id}` : id
+  fetch(`${baseEndpoint}/ai/tool/${toolId}`)
     .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
+
         renderModalData(allData.find(item => item.id == id))
         throw new Error("NETWORK RESPONSE ERROR");
       }
     })
     .then(data => {
+      console.log({ data })
       renderModalData(data?.data)
     })
     .catch((error) => console.error("FETCH ERROR:", error));
@@ -114,11 +141,14 @@ const renderAllCard = (data = []) => {
 
 const fetchToolsDataAndDisplay = (loadAllData = false) => {
   isLoadAllData = loadAllData
+  document.getElementById('loading').style.display = ""
+
   if (allData.length) {
     const toolsData = loadAllData ? allData : allData.slice(0, 6)
     renderAllCard(toolsData || [])
     if (loadAllData) {
       document.getElementById("loadMore").style.display = "none";
+      document.getElementsByClassName('loading').style.display = "none";
     }
   } else {
     fetch(`${baseEndpoint}/ai/tools`)
@@ -130,6 +160,7 @@ const fetchToolsDataAndDisplay = (loadAllData = false) => {
         }
       })
       .then(data => {
+        document.getElementById('loading').style.display = "none";
         allData = data?.data?.tools
         const toolsData = loadAllData ? data?.data?.tools : data?.data?.tools.slice(0, 6)
         renderAllCard(toolsData || [])
@@ -145,3 +176,8 @@ document.addEventListener("DOMContentLoaded", function () {
   fetchToolsDataAndDisplay();
 })
 
+
+const handleSortData = () => {
+  allData = allData.sort((itemOne, itemTwo) => new Date(itemOne.published_in) - new Date(itemTwo.published_in));
+  fetchToolsDataAndDisplay(isLoadAllData)
+}
